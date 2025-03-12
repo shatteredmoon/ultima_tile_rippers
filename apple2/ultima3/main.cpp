d68a8a24ee9a12019563b3ee07fa42803080de01
shatteredmoon <johnny.a.wood@gmail.com>
2025-03-11 23:09:19 -0400

    Adds support for extracting the character tile data from Apple2 Ultima3. Removes the ultima31.dsk image from the project. Adds the TEXT file, extracted from the .dsk image at offset 0x6300-0x66FF. Fixes the SHAPES file since it contained too much data. It now reflects the data extracted from the .dsk image at offset 0x5B00-0x62FF. Adds an example image of the extracted character data.

--- apple2/ultima3/main.cpp
+++ apple2/ultima3/main.cpp
@@ -1,6 +1,8 @@
 // Extracts Ultima III tile and text data from Apple ][ sources.
-// This program requires the ultima31.dsk image (the SHAPES here was extracted from that image starting at offset
-// 0x5B00 to 0x7AFF). Each 128 byte stride contains 1 row of data (2 bytes each) for each of the 64 tiles.
+// This program requires the ultima31.dsk image. The SHAPES file attached here was extracted from that image starting
+// at offset 0x5B00 to 0x62FF. Each 128 byte stride contains 1 row of tile data (2 bytes for each tile ) for each of
+// the 64 tiles. The TEXT file was extracted started at offset 0x6300 to 0x66FF. Each 128 bytes stride contains 1 row
+// of text data (1 byte for each character) for each of the 128 characters.
 // Apple II disk and file archive manager: https://a2ciderpress.com/
 
 // Resources:
@@ -28,6 +30,15 @@
 
 #define TILE_BUFFER_WIDTH  ( TILE_WIDTH * TILES_PER_ROW )
 #define TILE_BUFFER_HEIGHT ( TILE_HEIGHT * TILES_PER_COL )
+
+#define CHAR_WIDTH    7
+#define CHAR_HEIGHT   8
+#define NUM_CHARS     128
+#define CHARS_PER_COL 1
+#define CHARS_PER_ROW 128
+
+#define CHAR_BUFFER_WIDTH  ( CHAR_WIDTH * CHARS_PER_ROW )
+#define CHAR_BUFFER_HEIGHT ( CHAR_HEIGHT * CHARS_PER_COL )
 
 #define EXPORT_VERTICAL_STRIP 0
 
@@ -139,10 +150,7 @@ int32_t main()
     return -1;
   }
 
-  const uint32_t numBytesToRead{ TILES_PER_ROW * TILE_HEIGHT * TILE_BYTES_PER_ROW };
-
-  // Jump to the start of tile data
-  infile.seekg( 0, std::ios::beg );
+  uint32_t numBytesToRead{ TILES_PER_ROW * TILE_HEIGHT * TILE_BYTES_PER_ROW };
 
   uint32_t x{ 0 };
   uint32_t y{ 0 };
@@ -211,6 +219,84 @@ int32_t main()
   destroy_bitmap( backBuffer2 );
 #else
   save_pcx( "tiles.pcx", backBuffer, nullptr );
+#endif // EXPORT_VERTICAL_STRIP
+
+  destroy_bitmap( backBuffer );
+
+  // ---------------------
+  // Process text graphics
+  // ---------------------
+
+  backBuffer = create_bitmap( CHAR_BUFFER_WIDTH, CHAR_BUFFER_HEIGHT );
+
+  infile.open( "TEXT", std::ios::in | std::ios::binary );
+
+  if( !infile.is_open() )
+  {
+    return -1;
+  }
+
+  numBytesToRead = CHARS_PER_ROW * CHAR_HEIGHT;
+
+  x = 0;
+  y = 0;
+
+  currentBytes = 0;
+  lineNum = 0;
+
+  while( currentBytes < numBytesToRead )
+  {
+    uint8_t tileData{ static_cast<uint8_t>( infile.get() ) };
+
+    // Remove the color group bit
+    const bool colorGroup{ ( ( tileData >> 7 ) & 0x1 ) == 0 };
+
+    tileData &= 0x7f;
+
+    // Place the 7 pixels
+    Draw( backBuffer, x++, y, ( tileData >> 0 ) & 0x3, false, colorGroup, true );
+    Draw( backBuffer, x++, y, ( tileData >> 1 ) & 0x3, ( tileData >> 0 ) & 0x1, colorGroup, false );
+    Draw( backBuffer, x++, y, ( tileData >> 2 ) & 0x3, ( tileData >> 1 ) & 0x1, colorGroup, true );
+    Draw( backBuffer, x++, y, ( tileData >> 3 ) & 0x3, ( tileData >> 2 ) & 0x1, colorGroup, false );
+    Draw( backBuffer, x++, y, ( tileData >> 4 ) & 0x3, ( tileData >> 3 ) & 0x1, colorGroup, true );
+    Draw( backBuffer, x++, y, ( tileData >> 5 ) & 0x3, ( tileData >> 4 ) & 0x1, colorGroup, false );
+    Draw( backBuffer, x++, y, ( tileData >> 6 ) & 0x3, ( tileData >> 5 ) & 0x1, colorGroup, true );
+
+    if( x >= CHAR_BUFFER_WIDTH )
+    {
+      // Wrap to next line
+      x = 0;
+      y += 1;
+    }
+
+    ++currentBytes;
+  }
+
+  infile.close();
+
+  // Optionally create a vertical strip
+#if EXPORT_VERTICAL_STRIP
+  sourceRow = 0;
+  sourceCol = 0;
+
+  backBuffer2 = create_bitmap( CHAR_WIDTH, CHAR_HEIGHT * NUM_CHARS );
+  for( int32_t i{ 0 }; i < NUM_CHARS; ++i )
+  {
+    blit( backBuffer, backBuffer2, sourceCol, sourceRow, 0, i * CHAR_HEIGHT, CHAR_WIDTH, CHAR_HEIGHT );
+    sourceCol += CHAR_WIDTH;
+
+    if( sourceCol >= CHAR_BUFFER_WIDTH )
+    {
+      sourceCol = 0;
+      sourceRow += CHAR_HEIGHT;
+    }
+  }
+
+  save_pcx( "text.pcx", backBuffer2, nullptr );
+
+  destroy_bitmap( backBuffer2 );
+#else
+  save_pcx( "text.pcx", backBuffer, nullptr );
 #endif // EXPORT_VERTICAL_STRIP
 
   destroy_bitmap( backBuffer );
