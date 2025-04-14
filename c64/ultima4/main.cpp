f20cc3341d2c5ad3b1bc77af6960f724beca8a2b
shatteredmoon <johnny.a.wood@gmail.com>
2025-04-13 21:22:13 -0400

    Adds color support for C64 Ultima4 tile ripping. Adds colors.prg, ripped from .d64 data at offset 0x130b0. Adds sample C64 Ultima4 tile output.

--- c64/ultima4/main.cpp
+++ c64/ultima4/main.cpp
@@ -40,63 +40,63 @@ int32_t main()
     return -1;
   }
 
-  int32_t depth{ 0 };
-  if( ( depth = desktop_color_depth() ) != 0 )
-  {
-    depth = 16;
-  }
-
+  int32_t depth{ desktop_color_depth() };
   set_color_depth( depth );
+
   if( set_gfx_mode( GFX_AUTODETECT_WINDOWED, 1024, 768, 0, 0 ) != 0 )
   {
     allegro_message( "Failed to set graphics mode" );
     return -1;
   }
 
-  int32_t c64Colors[16] =
+  int32_t c64Colors[] =
   {
     makecol( 0x00, 0x00, 0x00 ), // Black
     makecol( 0xff, 0xff, 0xff ), // White
-    makecol( 0x88, 0x00, 0x00 ), // Red
-    makecol( 0xaa, 0xff, 0xee ), // Cyan
-    makecol( 0xcc, 0x44, 0xcc ), // Purple
-    makecol( 0x00, 0xcc, 0x55 ), // Green
-    makecol( 0x00, 0x00, 0xaa ), // Blue
-    makecol( 0xee, 0xee, 0x77 ), // Yellow
-    makecol( 0xdd, 0x88, 0x55 ), // Orange
-    makecol( 0x66, 0x44, 0x00 ), // Brown
-    makecol( 0xff, 0x77, 0x77 ), // Light Red
-    makecol( 0x33, 0x33, 0x33 ), // Dark Gray
-    makecol( 0x77, 0x77, 0x77 ), // Gray
-    makecol( 0xaa, 0xff, 0x66 ), // Light Green
-    makecol( 0x00, 0x88, 0xff ), // Light Blue
-    makecol( 0xbb, 0xbb, 0xbb )  // Light Gray
+    makecol( 0x93, 0x3a, 0x4c ), // Red
+    makecol( 0xb6, 0xfa, 0xfa ), // Cyan
+    makecol( 0xd2, 0x7d, 0xed ), // Purple
+    makecol( 0x6a, 0xcf, 0x6f ), // Green
+    makecol( 0x4f, 0x44, 0xd8 ), // Blue
+    makecol( 0xfb, 0xfb, 0x8b ), // Yellow
+    makecol( 0xd8, 0x9c, 0x5b ), // Orange
+    makecol( 0x7f, 0x53, 0x07 ), // Brown
+    makecol( 0xef, 0x83, 0x9f ), // Light Red
+    makecol( 0x57, 0x57, 0x53 ), // Dark Gray
+    makecol( 0x57, 0x57, 0x53 ), // Gray
+    makecol( 0xb7, 0xfb, 0xbf ), // Light Green
+    makecol( 0xa3, 0x97, 0xff ), // Light Blue
+    makecol( 0xa3, 0xa7, 0xa7 )  // Light Gray
   };
 
   BITMAP* backBuffer{ create_bitmap( TILE_BUFFER_WIDTH, TILE_BUFFER_HEIGHT ) };
 
   std::ifstream infile;
-  infile.open("shapes.prg", std::ios::binary);
 
+  infile.open( "colors.prg", std::ios::binary );
   if( !infile.is_open() )
   {
     return -1;
   }
 
-  // Tile colors offset
-  /*infile.seekg(0x130b0, std::ios::beg);
-
+  // The tile colors exist in the .d64 image at offset 0x130b0.
   int32_t tileColors[NUM_TILES];
   for( int32_t i = 0; i < NUM_TILES; ++i )
   {
-    tileColors[i] = infile.get();
-  }*/
+    tileColors[i] = ( infile.get() >> 4 );
+  }
 
-  // Tile data offset
-  //infile.seekg( 0x16700, std::ios::beg );
+  infile.close();
+
+  infile.open( "shapes.prg", std::ios::binary );
+  if( !infile.is_open() )
+  {
+    return -1;
+  }
 
   const int32_t bit{ 0x80 };
 
+  // Tile/shape data exists in the .d64 image at offset 0x16700.
   // Tiles are set up like this: The first 2 bytes represent the left-half and right half of the first tile, then the
   // next 2-bytes are for the very top of the second tile This continues until the first row of all tiles are read.
   // Each byte = 8 pixels.
@@ -104,7 +104,8 @@ int32_t main()
   {
     for( int32_t k = 0; k < QUADS_PER_ROW; ++k )
     {
-      const int32_t tileColor{ 1 }; //tileColors[k] };
+      const int32_t tileNum{ k / 2 };
+      const int32_t tileColor{ tileColors[tileNum] };
 
       for( int32_t j = 0; j < PIXELS_PER_QUAD; ++j )
       {
